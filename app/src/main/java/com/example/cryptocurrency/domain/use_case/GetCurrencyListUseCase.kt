24472bba2cbc52d50564914474fa4631d6703b2c
jatin <jatinsinghal404@gmail.com>
2024-04-18 19:55:37 +0530

    usecase setup

---
+++ app/src/main/java/com/example/cryptocurrency/domain/use_case/GetCurrencyListUseCase.kt
@@ -0,0 +1,34 @@
+package com.example.cryptocurrency.domain.use_case
+
+import com.example.cryptocurrency.data.remote.dto.CryptoCurrency
+import com.example.cryptocurrency.domain.repository.SimpleSwapRepository
+import com.example.cryptocurrency.util.Resources
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCurrencyListUseCase @Inject constructor(
+    private val repository: SimpleSwapRepository
+) {
+
+    operator fun invoke(): Flow<Resources<List<CryptoCurrency>>> {
+        return flow {
+            try {
+                emit(Resources.Loading())
+                val currencyList = repository.getAllCryptoCurrency()
+                emit(Resources.Success(currencyList))
+            } catch (e:HttpException) {
+                emit(
+                    Resources.Error(e.localizedMessage ?: "An unexpected error" )
+                )
+            } catch (e:IOException) {
+                emit(
+                    Resources.Error("Something went wrong, please try again later")
+                )
+            }
+        }
+    }
+
+}
